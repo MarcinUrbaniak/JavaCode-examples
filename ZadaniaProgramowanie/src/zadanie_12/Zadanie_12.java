0eff0b8740f162286b245aa662d21eea3f274634
MarcinUrbaniak <marcin.urbaniak@gmail.com>
2019-05-04 23:03:46 +0200

    zadanie 1.2 - 1.3

---
+++ ZadaniaProgramowanie/src/zadanie_12/Zadanie_12.java
@@ -0,0 +1,13 @@
+package zadanie_12;
+
+
+public class Zadanie_12 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Program wyświetla liczbę pi z dokładnością.");
+        System.out.printf("Pi = " + "%6.5f\n", Math.PI);
+
+
+    }
+}
---
+++ ZadaniaProgramowanie/src/zadanie_13/Zadanie_13.java
@@ -0,0 +1,11 @@
+package zadanie_13;
+
+public class Zadanie_13 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Program wyświetla pierwiastek kwadratwowy");
+        System.out.println("liczby pi z dokładnością do dwóch miejsc po przecinku.");
+        System.out.printf("Sqrt(Pi) = " + "%2.2f\n", Math.sqrt(Math.PI));
+    }
+}
