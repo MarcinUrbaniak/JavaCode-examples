269ad74a0555a3903dd0d5ee9f8be10a76d1c657
MarcinUrbaniak <marcin.urbaniak@gmail.com>
2019-05-06 18:25:22 +0200

    zadanie 1.7

---
+++ ZadaniaProgramowanie/src/zadanie_17/Zadanie_17.java
@@ -0,0 +1,44 @@
+package zadanie_17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+public class Zadanie_17 {
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         „Napisz program, który oblicza sumę, różnicę, iloczyn i iloraz dla dwóch liczb x i y wprowadzanych z klawiatury.
+         W programie należy założyć, że zmienne x i y są typu float (rzeczywistego).
+         Dla zmiennych x, y, suma, różnica, iloczyn i iloraz należy przyjąć format ich wyświetlania na ekranie z dokładnością
+         do dwóch miejsc po przecinku.”
+         */
+
+        float x, y, sum, difference, ratio, quoitent;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Podaj x: ");
+        x = Float.parseFloat(br.readLine());
+
+        System.out.println("Podaj y: ");
+
+        y = Float.parseFloat(br.readLine());
+
+
+        sum = x + y;
+        difference = x - y;
+        ratio = x * y;
+        quoitent = x/y;
+
+        System.out.printf("Suma wynosi " + "%2.2f\n", sum);
+        System.out.printf("Różnica wynosi " + "%2.2f\n", difference);
+        System.out.printf("Iloczyn wynosi " + "%2.2f\n", ratio);
+        System.out.printf("Iloraz wybosi " + "%2.2f\n", quoitent);
+
+
+
+    }
+}
