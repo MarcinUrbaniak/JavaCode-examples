dd81a0fd333eed0c949818a1a4b3ac0487eae303
MarcinUrbaniak <marcin.urbaniak@gmail.com>
2019-05-11 13:15:51 +0200

    Strumienie tworzenie mapy

--- StrumienieExample/src/Example2.java
+++ StrumienieExample/src/Example2.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Example2 {
@@ -16,15 +20,15 @@ public class Example2 {
         Stream<String> rows1 = Files.lines(Paths.get("data.txt"));
 
         int rowCount = (int) rows1.map(x -> x.split(","))
-        .filter(x -> x.length == 3)
-        .count();
+                .filter(x -> x.length == 3)
+                .count();
 
         rows1.close();
 
         System.out.println("rowCount = " + rowCount);
 
 /**
- * Teraz dodajemy warunek na 2 kolumnę x[1] z wykorzystaniem partowania do inta
+ * Teraz dodajemy warunek na 2 kolumnę x[1] z wykorzystaniem parsowania do inta
  */
 
         Stream<String> rows2 = Files.lines(Paths.get("data.txt"));
@@ -35,6 +39,28 @@ public class Example2 {
                 .forEach(x -> System.out.println(x[0] + " " + x[1] + " " + x[2]));
 
         rows2.close();
+
+/**
+ * Teraz z wykorzystaniem mapy
+ */
+        Map<String, Integer> map = new HashMap<>();
+        Stream<String> rows3 = Files.lines(Paths.get("data.txt"));
+
+        map = rows3.map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .filter(x -> Integer.parseInt(x[1]) > 15)
+                .collect(Collectors.toMap(x -> x[0], //metoda toMap tworzy kolekcję klucz wartość
+                        x -> Integer.parseInt(x[1])
+                ));
+
+        rows3.close();
+
+        for (String key: map.keySet()
+             ) {
+            System.out.println("key = " + key + " value = " + map.get(key));
+        }
+
+
 
     }
 }
