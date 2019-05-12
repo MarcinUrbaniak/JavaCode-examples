94bf38bb783be4b470ac2b2bdd69ef461986fbfa
MarcinUrbaniak <marcin.urbaniak@gmail.com>
2019-05-12 16:04:33 +0200

    Kolko krzyzyk code before refactor

--- KolkoKrzyzyk/src/Game.java
+++ KolkoKrzyzyk/src/Game.java
@@ -1,7 +1,6 @@
 public class Game {
 
 
-
     //Typ użytkownika, który gra
     PlayerType playerType1;
     PlayerType playerType2;
@@ -14,100 +13,49 @@ public class Game {
 
 
     public Game(PlayerType playerType1, PlayerType playerType2) {
-        //this.playerType1 = playerType1;
-        //this.playerType2 = playerType2;
-
-        if(playerType1.getValueOfPlayerType() == 0 && playerType2.getValueOfPlayerType() == 0){
-
-        while (board.checkBoard()) {
-            PrintBoard.printCurrentBoard(board);
-            System.out.println("Player 1 ");
-            board.getMoveHuman(player1);
-            System.out.println("czy jest zwyciezca player1? " + board.checkWinner());
-            if (board.checkWinner()){
-                System.out.println("Koniec gry");
-                PrintBoard.printCurrentBoard(board);
-                break;
-            }
-            if (!board.checkBoard()) {
-                break;
-            }
-            PrintBoard.printCurrentBoard(board);
-            System.out.println("Player 2 ");
-            board.getMoveHuman(player2);
-            System.out.println("czy jest zwyciezca player2? " + board.checkWinner());
-            if(board.checkWinner()){
-                System.out.println("Koniec gry");
-                PrintBoard.printCurrentBoard(board);
-                break;
-            }
-        }
-        }
-
-
-        if(playerType1.getValueOfPlayerType() == 1 && playerType2.getValueOfPlayerType() == 1){
-
-            while (board.checkBoard()) {
-            PrintBoard.printCurrentBoard(board);
-            System.out.println("Player 1 ");
-            board.getMoveComputerRandom(player1);
-            System.out.println("czy jest zwyciezca player1? " + board.checkWinner());
-            if (board.checkWinner()){
-                System.out.println("Koniec gry");
-                PrintBoard.printCurrentBoard(board);
-                break;
-            }
-            if (!board.checkBoard()) {
-                break;
-            }
-            PrintBoard.printCurrentBoard(board);
-            System.out.println("Player 2 ");
-            board.getMoveComputerRandom(player2);
-            System.out.println("czy jest zwyciezca player2? " + board.checkWinner());
-            if(board.checkWinner()){
-                System.out.println("Koniec gry");
-                PrintBoard.printCurrentBoard(board);
-                break;
-            }
-        }
-
-        }
-
-
-        if(playerType1.getValueOfPlayerType() == 0 && playerType2.getValueOfPlayerType() == 1){
-
-            while (board.checkBoard()) {
-                PrintBoard.printCurrentBoard(board);
-                System.out.println("Player 1 ");
-                board.getMoveHuman(player1);
-                System.out.println("czy jest zwyciezca player1? " + board.checkWinner());
-                if (board.checkWinner()){
-                    System.out.println("Koniec gry");
-                    PrintBoard.printCurrentBoard(board);
-                    break;
-                }
-                if (!board.checkBoard()) {
-                    break;
-                }
-                PrintBoard.printCurrentBoard(board);
-                System.out.println("Player 2 ");
-                board.getMoveComputerRandom(player2);
-                System.out.println("czy jest zwyciezca player2? " + board.checkWinner());
-                if(board.checkWinner()){
-                    System.out.println("Koniec gry");
-                    PrintBoard.printCurrentBoard(board);
-                    break;
-                }
-            }
-
-
-
-
-
-        }
+        gameMethod(playerType1, playerType2);
     }
 
 
+    private void gameMethod(PlayerType playerType1, PlayerType playerType2) {
+        int round = 1;
+        while (board.checkBoard()) {
+
+            System.out.println("runda " + round);
+            round ++;
+            PrintBoard.printCurrentBoard(board);
+            System.out.println("Player 1 ");
+            if (playerType1 == PlayerType.HUMAN) {
+                board.getMoveHuman(player1);
+            } else {
+                //board.setMoveComputerRandom(player1);
+                board.setMoveComputer(player1);
+            }
+            System.out.println("czy jest zwyciezca player1? " + board.checkWinner());
+            if (board.checkWinner()) {
+                System.out.println("Koniec gry");
+                PrintBoard.printCurrentBoard(board);
+                break;
+            }
+            if (!board.checkBoard()) {
+                break;
+            }
+            PrintBoard.printCurrentBoard(board);
+            System.out.println("Player 2 ");
+            if (playerType2 == PlayerType.HUMAN) {
+                board.getMoveHuman(player2);
+            } else {
+                //board.setMoveComputerRandom(player2);
+                board.setMoveComputer(player2);
+            }
+            System.out.println("czy jest zwyciezca player2? " + board.checkWinner());
+            if (board.checkWinner()) {
+                System.out.println("Koniec gry");
+                PrintBoard.printCurrentBoard(board);
+                break;
+            }
+        }
+    }
 
 
 }
--- KolkoKrzyzyk/src/Play.java
+++ KolkoKrzyzyk/src/Play.java
@@ -9,66 +9,6 @@ public class Play {
 
         Game game = new Game(playerType1, playerType2);
 
-
-//        Board board = new Board();
-//
-//
-//        Player player1 = Player.PLAYER1;
-//        Player player2 = Player.PLAYER2;
-//
-//        while (board.checkBoard()) {
-//            PrintBoard.printCurrentBoard(board);
-//            System.out.println("Player 1 ");
-//            board.getMoveHuman(player1);
-//            System.out.println("czy jest zwyciezca player1? " + board.checkWinner());
-//            if (board.checkWinner()){
-//                System.out.println("Koniec gry");
-//                PrintBoard.printCurrentBoard(board);
-//                break;
-//            }
-//            if (!board.checkBoard()) {
-//                break;
-//            }
-//            PrintBoard.printCurrentBoard(board);
-//            System.out.println("Player 2 ");
-//            board.getMoveHuman(player2);
-//            System.out.println("czy jest zwyciezca player2? " + board.checkWinner());
-//            if(board.checkWinner()){
-//                System.out.println("Koniec gry");
-//                PrintBoard.printCurrentBoard(board);
-//                break;
-//            }
-//        }
-
-/**
- * Kod testujący. Random komputer gra z random komputerem
- *
- *
- */
-//        while (board.checkBoard()) {
-//            PrintBoard.printCurrentBoard(board);
-//            System.out.println("Player 1 ");
-//            board.getMoveComputerRandom(player1);
-//            System.out.println("czy jest zwyciezca player1? " + board.checkWinner());
-//            if (board.checkWinner()){
-//                System.out.println("Koniec gry");
-//                PrintBoard.printCurrentBoard(board);
-//                break;
-//            }
-//            if (!board.checkBoard()) {
-//                break;
-//            }
-//            PrintBoard.printCurrentBoard(board);
-//            System.out.println("Player 2 ");
-//            board.getMoveComputerRandom(player2);
-//            System.out.println("czy jest zwyciezca player2? " + board.checkWinner());
-//            if(board.checkWinner()){
-//                System.out.println("Koniec gry");
-//                PrintBoard.printCurrentBoard(board);
-//                break;
-//            }
-//        }
-
     }
 
 
