122965ad5d203255973706262a9dc3b192b66579
MarcinUrbaniak <marcin.urbaniak@gmail.com>
2019-06-06 08:27:12 +0200

    Add raports class

---
+++ HomeBudget/src/homebudget/services/raports/ExpensesRaport.java
@@ -0,0 +1,13 @@
+package homebudget.services.raports;
+
+import homebudget.model.data.repository.Expenses;
+
+public class ExpensesRaport {
+
+
+    public static void perMonthAndCategory(Expenses expenses){
+
+        
+    }
+
+}
---
+++ HomeBudget/src/homebudget/services/raports/IncomesRaport.java
@@ -0,0 +1,13 @@
+package homebudget.services.raports;
+
+import homebudget.model.data.repository.Incomes;
+
+public class IncomesRaport {
+
+    public static void perMonthAndCategory(Incomes incomes){
+
+
+
+    }
+
+}
