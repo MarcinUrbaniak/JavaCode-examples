1c19f14d564cb5e804e98aff132e7666fba137d5
MarcinUrbaniak <marcin.urbaniak@gmail.com>
2019-07-18 08:28:26 +0200

    Lambda::Interfejs funcyjny Function - przyklad

---
+++ LambdaExcersise/src/demo/interf/function/UseFunctionInterfaceDemo.java
@@ -0,0 +1,18 @@
+package demo.interf.function;
+
+import java.util.function.Function;
+
+public class UseFunctionInterfaceDemo {
+    public static void main(String[] args) {
+        Function<Integer, Integer> factorial = (n) ->{
+            int result = 1;
+            for (int i = 1; i <=n ; i++) {
+                result = result * i;
+
+            }
+            return result;
+        };
+
+        System.out.println("Silnia liczny 5 wynosi: " + factorial.apply(5));
+    }
+}
